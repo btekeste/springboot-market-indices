e63ef0fbe4c4ca96c64a93a1b506b9c08b714c8e
Berhe Tekeste <12522087+btekeste@users.noreply.github.com>
2021-10-18 16:03:49 -0400

    Create IndexRepository.java

---
+++ src/main/java/com/app/index/IndexRepository.java
@@ -0,0 +1,18 @@
+package com.app.index;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+//DATA ACCESS LAYER
+//Data Access Object class
+@Repository
+public interface IndexRepository extends JpaRepository<Index, Long> {
+    
+    //JPQL language: SELECT * FROM student WHERE email = ?
+    @Query("SELECT s FROM Index s WHERE s.ticker = ?1") 
+    Optional<Index> findIndexByTicker(String ticker);
+
+}
